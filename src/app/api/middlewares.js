b5dd0864ee9cb424fb80aa406e8c700dd163fbfe
Pedro José Peña Jerez <malagactiva@gmail.com>
2016-10-02 22:53:23 +0200

    Add Cors

--- src/app/api/middlewares.js
+++ src/app/api/middlewares.js
@@ -1,7 +1,8 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export default function(app){
-
+    app.use(cors());
     app.use(bodyParser.json({limit: '50mb'}));
     app.use(bodyParser.urlencoded({extended:true, limit: '50mb'}));
 }
