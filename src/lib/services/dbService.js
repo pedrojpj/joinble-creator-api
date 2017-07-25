c2ca3766ba3f89eb48d1346c317b947244da586d
Pedro José Peña <pjpena@stratio.com>
2017-07-25 15:45:55 +0200

    refactor(Versions && DB Service): Update versions add DB Service

--- src/lib/services/dbService.js
+++ src/lib/services/dbService.js
@@ -7,37 +7,37 @@ const debug = require('debug')(config.APP_NAME);
 mongoose.Promise = Promise;
 
 class DbService {
-    constructor() {
-        this._connected = false;
-        this.retries = 0;
-        this.uri = config.DB_URI;
-    }
-    connected() {
-        return this._connected;
-    }
-    connect() {
-        mongoose.connect(this.uri);
+   constructor() {
+      this._connected = false;
+      this.retries = 0;
+      this.uri = config.DB_URI;
+   }
+   connected() {
+      return this._connected;
+   }
+   connect() {
+      mongoose.createConnection(this.uri, { useMongoClient: true });
 
-        mongoose.connection
-            .on('error', () => {
-                debug('mongodb connect error');
-                debug('try connect');
-            })
-            .on('open', (db) => {
-                debug('mongodb connected');
-                this._connected = true;
-            })
-            .on('disconnected', () => {
-                this._connected = false;
-                setTimeout(this.connect, 5000);
-            })
+      mongoose.connection
+         .on('error', () => {
+            debug('mongodb connect error');
+            debug('try connect');
+         })
+         .on('open', db => {
+            debug('mongodb connected');
+            this._connected = true;
+         })
+         .on('disconnected', () => {
+            this._connected = false;
+            setTimeout(this.connect, 5000);
+         });
 
-        mongoose.set('debug', config.DEBUG);
-    }
+      mongoose.set('debug', config.DEBUG);
+   }
 
-    disconnect() {
-        mongoose.disconnect();
-    }
+   disconnect() {
+      mongoose.disconnect();
+   }
 }
 
 export default new DbService();
